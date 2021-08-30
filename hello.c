02e45d984432ab18f397484aa1941434aff84b0f
AbdoKRS <aalkharusi4@huskers.unl.edu>
2021-08-30 02:54:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Abdul Rahman Al kharusi
+ *
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Abdul Rahman Al Kharusi\n");
+
+  return 0;
+}
